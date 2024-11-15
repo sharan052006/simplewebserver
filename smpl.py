ba16505d7bf97e5befe39630e71d076d96653e9a
sharan052006 <sharan052006@gmail.com>
2024-11-15 20:49:24 +0530

    Success

--- smpl.py
+++ smpl.py
@@ -1,28 +1,3 @@
-# EX01 Developing a Simple Webserver
-## Date:15-11-2024
-
-## AIM:
-To develop a simple webserver to serve html pages and display the configuration details of laptop.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation.
-
-### Step 2:
-Design of webserver workflow.
-
-### Step 3:
-Implementation using Python code.
-
-### Step 4:
-Serving the HTML pages.
-
-### Step 5:
-Testing the webserver.
-
-## PROGRAM:
-
-```
 from http.server import HTTPServer, BaseHTTPRequestHandler
 content = """
 
@@ -53,15 +28,3 @@ server_address = ('',8000)
 httpd = HTTPServer(server_address,myhandler)
 print("my webserver is running...")
 httpd.serve_forever()
-
-
-```
-
-## OUTPUT:
-
-![alt text](<Screenshot (10).png>)
-
-![alt text](<Screenshot (12).png>)
-
-## RESULT:
-The program for implementing simple webserver is executed successfully.
